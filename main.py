13a05ea696773dc28d4e4a9e90bf64f5865b789b
Mai_Max <62517731+Mai-Tubo@users.noreply.github.com>
2022-08-07 23:35:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import telebot
-import os
-from flask import Flask, request
 from newsbot import *
 
 TOKEN = '5482902514:AAFawjggplZ2t87mthMLQj-uebJPTkpkAB4'
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
 @bot.message_handler(content_types=['text', 'file'])
 def main(message):
@@ -15,21 +12,6 @@ def main(message):
         bot.reply_to(message, 'Йде обробка...')
         bot.send_message(message.from_user.id, football('football'))   
     else:
-        bot.send_message(message.from_user.id, 'Будь-ласка, введіть команду /news')
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://newsfootballduet-bot.herokuapp.com/' + TOKEN)
-    return "!", 200           
+        bot.send_message(message.from_user.id, 'Будь-ласка, введіть команду /news')          
   
-if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443))) 
+bot.polling(none_stop=True, interval=0)
