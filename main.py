a9bdabb1f48b9290d52f6c7cbc443507b6c8603c
Mai_Max <62517731+Mai-Tubo@users.noreply.github.com>
2022-08-07 23:18:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import telebot
+import os
+from flask import Flask, request
+from newsbot import *
+
+TOKEN = '5482902514:AAFawjggplZ2t87mthMLQj-uebJPTkpkAB4'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    if message.text == '/start':
+        bot.reply_to(message, 'Привіт {name}'.format(name=message.from_user.first_name))   
+    else:
+        bot.send_message(message.from_user.id, 'Будь-ласка, введіть команду /news')
+
+@bot.message_handler(content_types=['document'], commands=['news'])           
+def echo(message):
+    bot.reply_to(message, 'Йде обробка...')
+    bot.send_message(message.from_user.id, football('football'))
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://newsfootballduet-bot.herokuapp.com/' + TOKEN)
+    return "!", 200           
+  
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443))) 
